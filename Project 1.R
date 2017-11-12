ab2bc5124bfdbf9785d9478b0f496a994772e9dd
Farhan Choudhary <30686453+farhanchoudhary@users.noreply.github.com>
2017-11-12 14:47:00 +0530

    Project files

---
+++ Project 1.R
@@ -0,0 +1,5 @@
+#Statistical Inference Course Project
+##By Farhan Choudhary 
+
+###Set the working directory
+setwd("C:/Users/farhan.choudhary/Desktop/R/Johns Hopkins University/Statistical Inference")
